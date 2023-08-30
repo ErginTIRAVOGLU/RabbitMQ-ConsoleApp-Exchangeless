a353340e737b5b44bf8edb3de0ab1254035a58b4
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2023-08-30 12:45:26 +0300

    first commit

---
+++ UdemyRabbitMQ.Publisher/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("RabbitMQ Publisher");
+
+IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddEnvironmentVariables().Build();
+var section = config.GetSection("Settings");
+var rabbitMQUrl = section.GetValue<string>("RabbbitMQUrl");
+
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri(rabbitMQUrl);
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+string message = "Hello World";
+
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+Console.WriteLine("Mesaj Gönderilmiştir.");
+
+Console.ReadLine();
---
+++ UdemyRabbitMQ.Subscriber/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+Console.WriteLine("RabbitMQ Subscriber");
+
+IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddEnvironmentVariables().Build();
+var section = config.GetSection("Settings");
+var rabbitMQUrl = section.GetValue<string>("RabbbitMQUrl");
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri(rabbitMQUrl);
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello-queue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue", true, consumer);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine("Gelen Mesaj : " + message);
+};
+
+
+
+Console.ReadLine();
